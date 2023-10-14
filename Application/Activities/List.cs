b7fa579e7780fa6f60d7914b81ddbee39d33dfe6
stringskip <philiptesiorna@gmail.com>
2023-10-14 14:27:17 +0800

    My first commit again

---
+++ Application/Activities/List.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>>{}
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                List<Activity> rvalue = null;
+
+                try
+                {
+                    var activities = await _context.Activities.ToListAsync();
+                    if(activities != null && activities.Count > 0)
+                    {
+                        rvalue = activities;
+                    }
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+
+                return rvalue;
+            }
+        }
+    }
+}
